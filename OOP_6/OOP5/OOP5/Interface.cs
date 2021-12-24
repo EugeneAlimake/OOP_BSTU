4ecfe7d9085dace300cdfeac7159a85e3d93bbea
Eugene <90150437+EugeneAlimake@users.noreply.github.com>
2021-12-24 22:02:05 +0300

    Add files via upload

---
+++ OOP_6/OOP5/OOP5/Interface.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP5
+{
+    public interface Inventary
+    {
+        void Take_Invent();
+        void Back_Invent();
+    }
+
+    public interface InBall : Inventary
+    {
+        void Play();
+
+    }
+    public interface InTennisBall : InBall
+    {
+        void Do();
+    }
+    public interface InBacketBall : InBall
+    {
+        void Do();
+    }
+
+
+}
