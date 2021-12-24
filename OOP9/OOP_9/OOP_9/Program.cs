5a83345563136d4f910250221f266d9d04230ce6
Eugene <90150437+EugeneAlimake@users.noreply.github.com>
2021-12-24 22:28:41 +0300

    Add files via upload

---
+++ OOP9/OOP_9/OOP_9/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace OOP_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User.Software Software;
+            User user = new User();
+            List<string> SW = new List<string> { "Windows", "MacOS", "Linux", "bada", "UNIX" };
+            foreach (string item in SW)
+            {
+                Console.Write(item + "\t");
+            }
+            user.Upgrade += (list, message) =>
+            {
+                Console.WriteLine(message);
+                foreach (string item in list)
+                {
+                    Console.Write(item + "\t");
+                }
+            };
+            user.Work += (list, message) =>
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine(message);
+                Console.ResetColor();
+            };
+           
+            Software = user.Loading;
+            Software(SW, ""); 
+            Software = user.Edit;
+            Software(SW, "");
+            Console.WriteLine("\n\n\n--------------Работа со строками--------------\n");
+
+            Func<string, string> A;
+            string str = "Мяу , Мур, кис";
+
+            Console.WriteLine($"Исходная строка:        {str}");
+            A = StringHandler.RemoveS;
+            Console.WriteLine($"Без знаков препинания:  {str = A(str)}");
+            A = StringHandler.RemoveSpase;
+            Console.WriteLine($"Без пробелов:           {str = A(str)}");
+            A = StringHandler.Upper;
+            Console.WriteLine($"Заглавными буквами:     {str = A(str)}");
+            A = StringHandler.Lower;
+            Console.WriteLine($"Строчными буквами:      {str = A(str)}");
+            A = StringHandler.AddToString;
+
+            Console.WriteLine($"С добавлением символа:  {str = A(str)}");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ OOP9/OOP_9/OOP_9/User.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace OOP_9
+{
+    class User
+    {
+        public delegate void Software(List<string> list, string message);
+        public event Software Upgrade;
+        public event Software Work;
+        
+        public void Edit(List<string> list, string m)
+        {
+            Console.Write("\n Введите номер объекта (1 - 5), который хотите изменить: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Что вы хотите добавить?: ");
+            string name = Console.ReadLine();
+
+            list[number-1] += name;
+            Upgrade?.Invoke(list, "Изменено.\n");
+        }
+        public void Loading(List<string> list, string m)
+        {Console.Write("\nВведите номер объекта (1 - 5), который хотите запустить: ");
+            int Percentage = 0;
+             
+            
+            int number = Convert.ToInt32(Console.ReadLine());
+            Console.Write($"Загрузка ");
+            for (int i = 0; i <= 5; i++)
+            {
+                int top = 2;
+                Thread.Sleep(800);
+
+                int startIndex = Console.CursorLeft;
+                Console.Write($"{Percentage} %");
+                Console.SetCursorPosition(startIndex, top);
+                Percentage += 20;
+               
+            }
+            Console.Write($"{list[number - 1]}");
+
+            Work?.Invoke(list," Загружен");
+        }
+        
+    }
+}
